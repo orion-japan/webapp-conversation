9c14b32c810c594c51c6cbcacc372721ad73d036
orion-japan <arigatousun8@gmail.com>
2025-06-11 07:35:43 +0900

    Add start + new conversation logic with uid

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: false, // enable browser source map generation during the production build
-  // Configure pageExtensions to include md and mdx
+  productionBrowserSourceMaps: false,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     // appDir: true,
   },
-  // fix all before production. Now it slow the develop speed.
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
     ignoreBuildErrors: true,
+  },
+
+  // ✅ ここが追加ポイント
+  env: {
+    DIFY_API_KEY: process.env.DIFY_API_KEY,
   },
 }
 
